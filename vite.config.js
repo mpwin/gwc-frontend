60f552a34b8205c3f1839fe4f796d4e662677a42
Mateusz Winiarczyk <matthew.winiarczyk@gmail.com>
2025-08-28 13:23:20 +0200

    build: configure networking and API proxy

--- vite.config.js
+++ vite.config.js
@@ -3,4 +3,13 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-})
+  server: {
+    proxy: {
+      '/api': {
+        target: 'http://gwc-backend-web:8000',
+        changeOrigin: true,
+        secure: false,
+      },
+    },
+  },
+});
