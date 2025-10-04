c00f0bf548b83ffb330cc2def2f137fc4dfd1ef3
Mateusz Winiarczyk <matthew.winiarczyk@gmail.com>
2025-10-04 13:31:02 +0200

    feat: display zone collections

--- src/components/Zone.jsx
+++ src/components/Zone.jsx
@@ -1,4 +1,5 @@
 import { useState, useEffect } from 'react';
+import './Zone.css';
 
 export function Zone({ slug }) {
   const [data, setData] = useState({});
@@ -12,6 +13,47 @@ export function Zone({ slug }) {
   return (
     <div className="zone">
       <h2>{data.name}</h2>
+
+      <div className="collections">
+        <div className="achievement_collections">
+          {data.achievement_collections?.map(collection => (
+            <>
+              <div>{collection.name}</div>
+              <ul>
+                {collection.achievements.map(achievement => (
+                  <li>{achievement.name}</li>
+                ))}
+              </ul>
+            </>
+          ))}
+        </div>
+
+        <div className="skin_collections">
+          {data.skin_collections?.map(collection => (
+            <>
+              <div>{collection.name}</div>
+              <ul>
+                {collection.skins.map(skin => (
+                  <li>{skin.name}</li>
+                ))}
+              </ul>
+            </>
+          ))}
+        </div>
+
+        <div className="misc_collections">
+          {data.misc_collections?.map(collection => (
+            <>
+              <div>{collection.name}</div>
+              <ul>
+                {collection.items.map(item => (
+                  <li>{item.name}</li>
+                ))}
+              </ul>
+            </>
+          ))}
+        </div>
+      </div>
     </div>
   );
 }
